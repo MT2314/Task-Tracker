bc5ca63bc80a9012d10a95b447e4b33e17f81be1
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-02-21 18:19:39 -0500

    json-server setup, delete added

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
-  const [showAddTask,setShowAddTask] = useState(false); 
-  const[tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-])
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async() => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer);
+    }
+    getTasks()
+  }, [])
+
+  //Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+  //Add Tasks
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
 
-//Add Tasks
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks,newTask])
-}
+  //Delete Task
+  const deleteTask = async (id) => {
+    await  fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
 
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
-//Delete Task
-const deleteTask = (id) =>{
-  setTasks(tasks.filter((task) => task.id !==id))
-}
-
-//Toggle Reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id ===id?{ ...task, reminder:!task.reminder} : task))
-}
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
 
   return (
     <div className="container">
-      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
-      {showAddTask && <AddTask onAdd = {addTask}/>}
-      {tasks.length > 0 ?<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} /> : 'No Tasks To Show'}
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
